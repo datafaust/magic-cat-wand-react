f12043ecc969641888e93f4941029057c0ff0e73
mac <mac@macs-MBP.cable.rcn.com>
2022-04-04 11:17:17 -0400

    add edits

--- ip_bank/linux_get_ip.sh
+++ ip_bank/linux_get_ip.sh
@@ -1,4 +1,14 @@
- #!/bin/bash
+#!/bin/bash
 
+# test existence of a directory
+cd ./magic-cat-wand-react/ip_bank/
+
+# if a directory exists create an env file
+touch .env
+
+# assign the ip address to that env file
 MACHINE_HOST_IP="$(hostname -I | cut -d " " -f 2)"
 echo "${MACHINE_HOST_IP}"
+echo "$(ls)"
+destdir=.env
+echo "MACHINE_HOST_IP=$MACHINE_HOST_IP" > "$destdir"
