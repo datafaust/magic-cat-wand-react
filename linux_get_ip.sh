d898bca5b722398cfab703e11e70d81ac8a5a07f
mac <mac@macs-MBP.cable.rcn.com>
2022-04-04 13:43:59 -0400

    fix sh run

--- linux_get_ip.sh
+++ linux_get_ip.sh
@@ -4,11 +4,11 @@
 cd /home/pi/magic-cat-wand-react/
 
 # if a directory exists create an env file
-touch .env
+touch /home/pi/magic-cat-wand-react/.env
 
 # assign the ip address to that env file
 MACHINE_HOST_IP="$(hostname -I | cut -d " " -f 2)"
 echo "${MACHINE_HOST_IP}"
 echo "$(ls)"
-destdir=.env
+destdir=/home/pi/magic-cat-wand-react/.env
 echo "MACHINE_HOST_IP=$MACHINE_HOST_IP" > "$destdir"
